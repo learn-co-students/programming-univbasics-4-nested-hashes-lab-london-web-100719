355822e724b669fd78ff31a283e7c8756aad6b9d
Qing Wang <chingwang.qw@gmail.com>
2019-09-20 16:51:19 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,6 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
 end
 
 def alan_kay_is_known_for
@@ -37,6 +35,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  return  programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -63,7 +63,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -80,7 +79,10 @@ def adding_matz
         }
      }
 
+  programmer_hash = [:yukihiro_matsumoto => { :known_for => "Ruby", :languages => ["LISP", "C"]] 
     
+  return programmer_hash
+  
 end
 
 def changing_alan
@@ -99,11 +101,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay] [:known_for] = [:alans_new_info]["GUI"]
+     
+     return programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  return programmer_hash
 end
